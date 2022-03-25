1407fa1ad7fe327147744ce13311638aa2ed68d1
akeilah8 <97969321+akeilah8@users.noreply.github.com>
2022-03-25 11:09:10 -0400

    Add files via upload

---
+++ example.js
@@ -0,0 +1,35 @@
+// Create variables for the welcome message
+var greeting = 'Howdy';
+var name = 'Molly';
+var message = ',please check your order:';
+//Concatenate variables to hold details about the sign
+var sign = 'Montague House';
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+
+//Get the element that has an id of greeting 
+var el = document.getElementById('greeting');
+// Replace the content of that element of that element with personlized welcome message
+el.textContent = welcome;
+
+//Get the element that has an id of userSign then update its contents
+var elSign = document.getElementById('userSign');
+elSign.textContent = sign;
+
+// Get the element that has an id of tiles then update its contents
+var elTiles = document.getElementById('tiles');
+elTiles.textContent = tiles;
+
+//Get the element that has an id of subTotal then update its contents
+var elSubTotal = document.getElementById('subTotal');
+elSubTotal.textContent = '$' + subTotal;
+
+//Get the element that has an id of shipping then update its contents
+var elSubTotal = document.getElementById('shipping');
+elSubTotal.textContent = '$' + shipping;
+
+//Get the element that has an id of grandTotal then update its contents
+var elGrandTotal = document.getElementById('grandTotal');
+elGrandTotal.textContent = '$' + grandTotal;
